24658a99a547c29499102224fffd27e80845512d
guimatera <guilhermematera@ejcm.com.br>
2021-12-04 12:45:10 -0300

    Adicionando token ao código

--- orotera.py
+++ orotera.py
@@ -2,7 +2,13 @@
 import discord
 from discord.ext import commands
 
+# Credenciais
+Token = "OTE2NzA2MjU4NjU0OTg2Mjgw.YauDOw.9qMaYiF71_v6BrATEV3on0QoAeo"
+
 bot = commands.Bot(command_prefix='%')
+
+
+
 
 
 
